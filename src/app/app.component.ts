ee1ff9286c158a94aa4427fa0d470041b5afef88
AsiaDrew <asiadrew2@outlook.com>
2022-08-07 13:21:12 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularToDo';
+  title = 'AngularToDo'; 
+  todoArray: Todo[] = [ 
+    {task: "mow grass", completed: false},
+    {task: "get gas", completed: true},
+    {task: "wash dishes", completed: false},
+    {task: "fold laundry", completed: true},
+    {task: "cook dinner", completed: true}
+  ];
+  searchedArray: Todo[] = this.todoArray; 
+
+  newTask: Todo = {task: "", completed: false}; 
+  searchArrayByString: string = ""; 
+  todoArrayIndex: number = -1; 
+  
+
+  addTodo() {
+    let task = Object.assign({}, this.newTask);
+    this.todoArray.push(task);
+
+    this.searchArray(); 
+    this.newTask.task = ""; 
+  }
+
+  completeTask(index: number) {
+    this.getTodoArrayIndex(index);
+
+    this.todoArray[this.todoArrayIndex].completed = true;
+  }
+
+  removeTask(index: number) {
+    
+    this.getTodoArrayIndex(index);
+    
+    this.todoArray.splice(this.todoArrayIndex, 1);
+
+    this.searchArray();
+  }
+
+  searchArray() {
+    this.searchedArray = this.todoArray;
+
+    this.searchedArray = this.todoArray.filter((todo) => {
+      
+      return todo.task.includes(this.searchArrayByString);
+    });
+  }
+
+  getTodoArrayIndex(index: number) {  
+    this.todoArrayIndex = this.todoArray.findIndex((todo) => { 
+      return this.searchedArray[index] === todo;
+    });
+  }
+  
+  ngOnInit(): void {
+  }
 }
